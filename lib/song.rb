f8bbc5c49d23778c12038b3346426e04738492e0
Bala Luguja <bluguja@gmail.com>
2020-04-20 15:05:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -75,15 +75,6 @@ end
 
 
 
-#song.artist_name #=> "Taylor Swift"
-
-
-
-
-
-
-
-
 #this is for the find_or_create_by_name method
 song_1 = Song.find_or_create_by_name("Blank Space")
 song_2 = Song.find_or_create_by_name("Blank Space")
